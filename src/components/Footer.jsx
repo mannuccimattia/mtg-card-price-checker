4bd37d61bb9ce48e67d6adcecd8add064d75b788
mannuccimattia <mannuccimattia@gmail.com>
2025-10-13 18:39:11 +0200

    [UPD] Added svg logos and footer disclaimer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 const Footer = () => {
     return (
-        <footer className='text-secondary d-flex align-items-center justify-content-center'>
+        <footer className='text-secondary d-flex flex-column align-items-center justify-content-center'>
+            <small className="m-0 p-0 mb-1 text-center">
+                Portions of this website are unofficial Fan Content permitted under the Wizards of the Coast Fan Content Policy. The literal and graphical information presented on this site about Magic: The Gathering, including card images and mana symbols, is copyright Wizards of the Coast, LLC. This website is not produced by or endorsed by Wizards of the Coast.
+            </small>
             <div className='m-0 p-0'>
                 <em>
                     Personal project made by: <a className='nav-link d-inline' href="https://github.com/mannuccimattia"><i className="fa-brands fa-github"></i> mannuccimattia</a>
